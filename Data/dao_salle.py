4572ba49aa5e515a5aa139886e4247af50c44e8f
mansouri mohamed <mns09moh@gmail.com>
2026-04-18 19:08:29 -0400

    Add insert salle method

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -8,3 +8,13 @@ class DataSalle:
         with open("Data/config.json", "r", encoding="utf-8") as file:
             config = json.load(file)
         return mysql.connector.connect(**config)
+
+    def insert_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        query = "INSERT INTO salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+        values = (salle.code, salle.libelle, salle.type, salle.capacite)
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
